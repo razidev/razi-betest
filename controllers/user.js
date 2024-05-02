697b52a5b85b4794d12076b553c6ffcbf3262628
razisyahputro <razisyahputro@gmail.com>
2024-05-02 17:04:10 +0700

    add logic signin

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-
+const redisClient  = require('../middlewares/redis_connect');
 const User = require('../models/user');
 
 exports.signupUser = async (req, res, next) => {
@@ -22,7 +22,34 @@ exports.signupUser = async (req, res, next) => {
 
         return res.status(201).json({ message: 'User created successfully'})
     } catch (error) {
-        console.log('signup-err', error);
+        return next(error);
+    }
+};
+
+exports.signinUser = async (req, res, next) => {
+    try {
+        const findUser = await User.findOne({ emailAddress: req.body.email });
+        if (!findUser) {
+            return res.status(400).json({ message: 'Please input the correct email or password' });
+        }
+        const comparePassword = await bcrypt.compare(req.body.password, findUser.password);
+        if(!comparePassword) {
+            return res.status(400).json({ message: 'Please input the correct email or password' });
+        }
+
+        const payloadJwt = {
+            id: findUser._id.toString(),
+            email: findUser.emailAddress,
+            username: findUser.userName,
+            identity_number: findUser.identityNumber
+        }
+
+        await redisClient.set(findUser.emailAddress,  JSON.stringify(payloadJwt), { EX: (60 * 60 * 1)})
+        const accessToken = jwt.sign(payloadJwt, process.env.JWT_SECRET_KEY, { expiresIn: '1h'});
+
+        return res.status(200).json({ data: { token: accessToken }})
+    } catch (error) {
+        console.log('signin-err', error);
         return next(error);
     }
 };
--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,9 @@ const errorHandlingMiddleware = require('../middlewares/error_handling');
 const router = express.Router();
 
 
-router.post('/signup', userController.signupUser)
+router.post('/signup', userController.signupUser);
+router.post('/signin', userController.signinUser);
+
 
 router.use(errorHandlingMiddleware);
 
