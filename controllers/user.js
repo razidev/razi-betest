c5962a65801a90d8f472ca8136cd39e681db1f00
razisyahputro <razisyahputro@gmail.com>
2024-05-02 20:02:30 +0700

    add logic crud

--- controllers/user.js
+++ controllers/user.js
@@ -53,3 +53,107 @@ exports.signinUser = async (req, res, next) => {
         return next(error);
     }
 };
+
+exports.getUsers = async (req, res, next) => {
+    try {
+        let findAllUsers = await User.find();
+        findAllUsers = findAllUsers.map(e => {
+            return {
+                user_name: e.userName,
+                account_number: e.accountNumber,
+                email: e.emailAddress,
+                identity_number: e.identityNumber
+            }
+        })
+        return res.status(200).json({ data: findAllUsers })
+    } catch (error) {
+        return next(error);
+    }
+};
+
+exports.getUserByAccountNumber = async (req, res, next) => {
+    try {
+        const findByAccountNumber = await User.findOne({ accountNumber: req.params.account_number });
+        console.log(findByAccountNumber)
+
+        if (!findByAccountNumber) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        return res.status(200).json({ data: {
+            user_name: findByAccountNumber.userName,
+            account_number: findByAccountNumber.accountNumber,
+            email: findByAccountNumber.emailAddress,
+            identity_number: findByAccountNumber.identityNumber
+        }});
+    } catch (error) {
+        console.log('get-err', error);
+        return next(error);
+    }
+};
+
+exports.getUserByIdentityNumber = async (req, res, next) => {
+    try {
+        const findByIdentityNumber = await User.findOne({ identityNumber: req.params.identity_number });
+
+        if (!findByIdentityNumber) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        return res.status(200).json({ data: {
+            user_name: findByIdentityNumber.userName,
+            account_number: findByIdentityNumber.accountNumber,
+            email: findByIdentityNumber.emailAddress,
+            identity_number: findByIdentityNumber.identityNumber
+        }});
+    } catch (error) {
+        console.log('get-err', error);
+        return next(error);
+    }
+};
+
+exports.updateUser = async (req, res, next) => {
+    try {
+        let findUser = await User.findOne({ emailAddress: req.session.email });
+        if (!findUser) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        const hashedPassword = await bcrypt.hash(req.body.password, 12);
+        findUser.password = hashedPassword
+        console.log(findUser)
+        await findUser.save();
+        await redisClient.del(req.session.email) 
+
+        return res.status(200).json({ message: 'User updated successfully' })
+    } catch (error) {
+        console.log('update-err', error);
+        return next(error);
+    }
+};
+
+exports.removeUser = async (req, res, next) => {
+    try {
+        let findUser = await User.findOne({ emailAddress: req.session.email });
+        if (!findUser) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        await findUser.deleteOne({ emailAddress: req.session.email });
+        await redisClient.del(req.session.email)
+
+        return res.status(200).json({ message: 'User removed successfully' })
+    } catch (error) {
+        console.log('remove-err', error);
+        return next(error);
+    }
+};
+
+exports.signoutUser = async (req, res, next) => {
+    try {
+        
+    } catch (error) {
+        console.log('remove-err', error);
+        return next(error);
+    }
+};
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const userController = require('../controllers/user');
 const errorHandlingMiddleware = require('../middlewares/error_handling');
+const sessionMiddleware = require('../middlewares/session');
 const router = express.Router();
 
 
 router.post('/signup', userController.signupUser);
 router.post('/signin', userController.signinUser);
-
+router.get('/:account_number/account-number', [sessionMiddleware], userController.getUserByAccountNumber);
+router.get('/:identity_number/identity-number', [sessionMiddleware], userController.getUserByIdentityNumber);
+router.get('/', [sessionMiddleware], userController.getUsers);
+router.put('/update-password', [sessionMiddleware], userController.updateUser);
+router.delete('/remove', [sessionMiddleware], userController.removeUser);
 
 router.use(errorHandlingMiddleware);
 
